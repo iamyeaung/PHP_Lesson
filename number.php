76cbbf7615f84fec68e885dc4d38d97efa1970e2
Ye Aung <khunyeaung@gmai.com>
2025-01-30 21:01:28 +0630

    number

---
+++ number.php
@@ -0,0 +1,52 @@
+<?php
+    $firt_number = 600;
+    $second_number = 400;
+
+    $add = $firt_number + $second_number;
+    $sub = $firt_number - $second_number;
+    $mul = $firt_number * $second_number;
+    $div = $firt_number / $second_number;
+    $mod = $firt_number % $second_number;
+
+    echo "$add <br> $sub <br> $mul <br> $div <br> $mod";
+
+// Arithmetic Operators
+// Addition (+)
+// Subtraction (-)
+// Multiplication (*)
+// Division (/)
+// Modulus (%)
+// Increment (++)
+// Decrement (--)
+
+// Assignment Operators
+// Equal (=)
+// Plus Equal (+=)
+// Minus Equal (-=)
+// Multiple Equal (*=)
+// Divide Equal (/=)
+
+// Comparison Operators
+// Equal to (==)
+// Not Equal (!=)
+// Greater than (>)
+// Less than (<)
+// Greater than or equal (>=)
+// Less than or equal (<=)
+
+// Logical Operators
+// AND (&&)
+// OR (||)
+// NOT (!)
+
+// Bitwise Operators
+// AND (&)
+// OR (|)
+// XOR (^)
+// NOT (~)
+// Left shift (<<)
+// Right shift (>>)
+
+// String Operators
+// Concatenation (+)
+// Concat assignment (+=)
